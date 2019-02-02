c9a650549d539be8f73dc155cffef3f4f56d9fac
Kiran Tambe <42598239+kirantambe85@users.noreply.github.com>
2019-02-02 15:22:30 +0530

    ui

---
+++ Angular-JS/Angular-JS Complete-Template/01_Angular_First_App/01_app.js
@@ -0,0 +1,8 @@
+// Create Angular JS Module
+var app = angular.module('FirstApp',[]);
+
+// Create Controller
+app.controller('FirstAppCtrl',function ($scope) {
+    $scope.user='';
+
+});
---
+++ Angular-JS/Angular-JS Complete-Template/02_Angular_Ng_Init_Ng_If/02_app.js
@@ -0,0 +1,7 @@
+// Create ANgular Module
+var app=angular.module('AgeSelectionApp',[]);
+
+//Create a Controller
+app.controller('AgeSelectionAppCtrl',function ($scope) {
+    $scope.check=null;
+});
